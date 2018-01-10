9b7652ff9e425bc619a262d3218868fd8e2ad8ff
Michael Burak <michaelrburak@gmail.com>
2018-01-10 07:39:10 -0500

    added serializer for Note and responding to JSON on controller view

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,8 @@
 class NotesController < ApplicationController
   def index
+    @notes = Note.all
+    respond_to do |format|
+      format.html {render :show}
+      format.json {render json: @notes}
   end
 end
