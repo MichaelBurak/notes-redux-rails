b16af8f5940607fb0de400a3cf27bcdebc2fd791
Michael Burak <michaelrburak@gmail.com>
2018-01-17 16:53:26 -0500

    working on editing note form and properly passing in state and props

--- client/src/containers/Note/EditNoteContainer.js
+++ client/src/containers/Note/EditNoteContainer.js
@@ -1,20 +1,42 @@
 //allows editing of single note
 import React from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux'
-import * as actions from '../../store/actions/noteActions.js'
+import { bindActionCreators } from 'redux';
+import * as actions from '../../store/actions/noteActions.js';
+import NoteShowContainer from './NoteShowContainer'
 
 
 class EditNoteContainer extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      title: '',
+      body: ''
+    };
+  }
+
+  changeText(e) {
+    this.setState({
+      title: e.target.value
+    });
+  }
+
+  changeBody(e){
+    this.setState({
+      body: e.target.value
+    })
+  }
+
   render() {
     return (
       <div>
       <h2> Edit the post! </h2>
-      <form>
+      <form onSubmit={(e) => this.submitForm(e)}>
       Post Title:<br/>
-      <input type="text" name="title"/> <br/>
+      <input type="text" name="title" value={this.state.title} onChange={(e) => this.changeText(e)}/> <br/>
       Post Content:<br/>
-      <input type="text" name="body"/>
+      <input type="text" name="body" value={this.state.body} onChange={(e) => this.changeBody(e)}/>
       <input type="submit"/>
       </form>
       </div>
@@ -23,9 +45,11 @@ class EditNoteContainer extends React.Component {
   }
 
   function mapStateToProps(state,ownProps) {
-    const note = state.notes.notes.find(note => note.id == ownProps.match.params.id)
+    debugger
+    const note = {title: state.notes.notes.find(note => note.id == ownProps.match.params.id).title, body: state.notes.notes.find(note => note.id == ownProps.match.params.id).body}
     if (note) {
-      return { note }
+      debugger
+      return {note}
     } else {
       return { note: {} }
     }
