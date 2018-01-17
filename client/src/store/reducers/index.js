778a59f697aba3b42fe369786e3fbcae64257bf5
Michael Burak <michaelrburak@gmail.com>
2018-01-17 16:59:56 -0500

    starting to add redux-form

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import notesReducer from './notes_reducer';
-
+import {reducer as formReducer} from 'redux-form'
 
 const rootReducer =  combineReducers({
-  notes: notesReducer
+  notes: notesReducer,
+  form: formReducer
 });
 
 
