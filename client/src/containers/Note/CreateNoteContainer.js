3db9b1e30625e89353013dd49ab36a7828e9f1db
Michael Burak <michaelrburak@gmail.com>
2018-01-17 12:21:31 -0500

    react dynamic form input implemented in note creation form

--- client/src/containers/Note/CreateNoteContainer.js
+++ client/src/containers/Note/CreateNoteContainer.js
@@ -1,21 +1,42 @@
 //handles creation of a single note with autosave through throttle
-
-//allows editing of single note
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'
 import * as actions from '../../store/actions/noteActions.js'
 
 class CreateNoteContainer extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      text: '',
+      body: ''
+    };
+  }
+
+  changeText(e) {
+    this.setState({
+      text: e.target.value
+    });
+  }
+
+  changeBody(e){
+    this.setState({
+      body: e.target.value
+    })
+  }
+
+
   render() {
     return (
       <div>
       <form>
       <h2> Create a New Post! </h2>
       Post Title:<br/>
-      <input type="text" name="title"/> <br/>
+      <input type="text" name="title" value={this.state.title} onChange={(e) => this.changeText(e)}/> <br/>
       Post Content:<br/>
-      <input type="text" name="body"/>
+      <input type="text" name="body" value={this.state.body} onChange={(e) => this.changeBody(e)}/>
       <input type="submit"/>
       </form>
       </div>
