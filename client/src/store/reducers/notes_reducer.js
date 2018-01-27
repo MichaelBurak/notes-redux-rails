8f2bd98dc8cc79c622a04aafed3692669353a0c5
Michael Burak <michaelrburak@gmail.com>
2018-01-27 13:56:19 -0500

    removed all references to loading in reducer. trying to make smaller commits. thus one line.

--- client/src/store/reducers/notes_reducer.js
+++ client/src/store/reducers/notes_reducer.js
@@ -1,7 +1,7 @@
 export default function notesReducer(state= { notes: []}, action) {
   switch ( action.type ) {
     case 'FETCH_NOTES':
-      return {loading: false, notes: action.payload}
+      return {notes: action.payload}
     default:
       return state;
   }
