cf10ecc301f86b539f07189e0f12a05fc2ea8844
Michael Burak <michaelrburak@gmail.com>
2018-01-30 13:51:05 -0500

    removed other reducer reference from root reducer

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,9 +1,6 @@
-import { combineReducers } from 'redux';
-import notesReducer from './notes_reducer';
-import {reducer as formReducer} from 'redux-form'
+import {combineReducers} from 'redux';
+import notesReducer from './notesReducer';
 
-const rootReducer =  combineReducers({
-  notes: notesReducer
-});
+const rootReducer = combineReducers({notes: notesReducer});
 
 export default rootReducer;
