da3a571eb81b6a8e8b692d185518fe0ea3eaa6ea
Michael Burak <michaelrburak@gmail.com>
2018-01-30 17:18:39 -0500

    changes on formatting of text after editing and updating notes to look a bit nicer

--- client/src/components/DeletedNote.js
+++ client/src/components/DeletedNote.js
@@ -5,7 +5,7 @@ import React from 'react'
 
 const DeletedNote = ({note}) =>
   <div>
-    <h1> You have deleted your note! Here is what you deleted: </h1>
+    <h1> You have deleted your note! Here is what you deleted - </h1>
     <h3>Title: {note.title}</h3>
     <p>{note.body}</p>
     This page will re-render in 10 seconds.
--- client/src/components/UpdatedNote.js
+++ client/src/components/UpdatedNote.js
@@ -5,7 +5,7 @@ import React from 'react'
 
 const UpdatedNote = ({note}) =>
   <div>
-    <h1> You have updated your note! Here is the updated note: </h1>
+    <h1> You have updated your note! Here is the updated note - </h1>
     <h3>Title: {note.title}</h3>
     <p>{note.body}</p>
   </div>
