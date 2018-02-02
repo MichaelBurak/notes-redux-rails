cd23f5635fd28b1730c7dc42e12f7b4af570a3c1
Michael Burak <michaelrburak@gmail.com>
2018-02-02 10:11:24 -0500

    entire app is connected + fetches notes to stay aware of store

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,16 @@ import CreateNoteContainer from './containers/Note/CreateNoteContainer';
 import UpdatedNoteShowContainer from './containers/Note/UpdatedNoteShowContainer';
 import DeletedNoteContainer from './containers/Note/DeletedNoteContainer'
 import NavigationBar from './components/NavigationBar'
+import {bindActionCreators} from 'redux';
+import * as actions from './store/actions/noteActions';
 
 class App extends Component {
+
+  componentWillMount() {
+  if (this.props.notes.length === 0) {
+    this.props.actions.fetchNotes()
+  }
+}
 
   render() {
     return (
@@ -31,4 +39,19 @@ class App extends Component {
   }
 }
 
-export default connect(null)(App)
+//Gives access to notes in store.
+
+function mapStateToProps(state) {
+  return {notes: state.notePad.notes}
+}
+
+//Gives access to actions, specifically fetchNotes() in this case.
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
