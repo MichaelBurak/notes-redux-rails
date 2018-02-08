e188794b09ff8d14160155ee4295cd441b2100a7
Michael Burak <michaelrburak@gmail.com>
2018-02-08 12:02:32 -0500

    Navbar now uses Link tos with router instead of actual links, maintaining SPA.

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,18 +1,19 @@
 //Presentational component:
-//Displays simple links to index and create route.
+//Displays simple links to index and create route on all pages.
 
 import {Navbar, NavItem, Nav} from 'react-bootstrap'
 import React from 'react'
+import {withRouter, Link} from "react-router-dom"
 
-const NavigationBar = (props) =>
+const NavigationBar = () =>
 
 <Navbar>
   <Nav>
-    <NavItem eventKey={1} href="/">
-      Return to Index
+    <NavItem eventKey={1}>
+    <Link to={"/"}>Return to Index</Link>
     </NavItem>
-    <NavItem eventKey={2} href="/notes/new">
-    Create a new note!
+    <NavItem eventKey={2}>
+    <Link to={"/notes/new"}>Create a new note!</Link>
     </NavItem>
   </Nav>
 </Navbar>;
