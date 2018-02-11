d99d43bebbb5787d8c00c96df8dfd0b075b69438
Michael Burak <michaelrburak@gmail.com>
2018-02-11 16:18:35 -0500

    NoteShowContainer changed from class to const syntax.

--- client/src/containers/Note/NoteShowContainer.js
+++ client/src/containers/Note/NoteShowContainer.js
@@ -5,9 +5,16 @@ import { connect } from 'react-redux';
 //Introspects through router for corresponding note to URL path, passes down
 //appropriate data as note constant to Note presentational. 
 
-const NoteShowContainer = ({ note }) =>
+const NoteShowContainer = (props) => 
+
+< div > {
+  props.loading
+    ? <h1>Loading page...</h1>
+    :
 <Note
-note = {note}/>
+note = {props.note}/>
+}
+</div>
 
 const mapStateToProps = (state, ownProps) => {
   const note = state.notePad.notes.find(note => note.id == ownProps.match.params.id)
