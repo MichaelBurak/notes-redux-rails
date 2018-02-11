7fe28e5e4c181eae36f35c27c7e836439081454d
Michael Burak <michaelrburak@gmail.com>
2018-02-11 16:08:40 -0500

    Not yet implemented possible alert component for if errors are present in store.

---
+++ client/src/components/Error.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+import { Alert } from 'reactstrap' 
+
+const Error = (props) => 
+    <div>
+    <Alert color="warning"> 
+    Something has gone wrong! Terribly sorry!
+    </Alert>
+    </div>
+
+
+export default Error 
