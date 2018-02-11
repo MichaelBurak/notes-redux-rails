a930a5a37b12aee2af649f32216679e10ce9af4f
Michael Burak <michaelrburak@gmail.com>
2018-02-11 16:12:18 -0500

    removal of autosave attempt, retrying later, switched out grid styling and simply added button on editing notes.

--- client/src/containers/Note/EditNoteContainer.js
+++ client/src/containers/Note/EditNoteContainer.js
@@ -5,7 +5,7 @@ import { bindActionCreators } from 'redux'
 import * as actions from '../../store/actions/noteActions.js'
 import { connect } from 'react-redux';
 import { withRouter } from "react-router-dom"
-import {Grid, Row, Col} from 'react-bootstrap'
+import {Button} from 'reactstrap'
 
 class EditNoteContainer extends React.Component {
   //Sets state to include props and all data in state as empty strings.
@@ -20,8 +20,7 @@ class EditNoteContainer extends React.Component {
 }
 
   //When component will mount, takes in passed in props from Note presentational, 
-  //received from NotesIndex, to set state to proper note being edited's data. 
-  //Wait shortly for all elements to be set and then start checking for changes.
+  //received from NotesIndex, to set state to proper note being edited's data.
 
   componentDidMount() {
     this.setState({
@@ -29,7 +28,6 @@ class EditNoteContainer extends React.Component {
       title: this.props.title,
       body: this.props.body
     })
-    setTimeout(()=> this.startCycle(), 300)
     }
     
 
@@ -40,34 +38,6 @@ class EditNoteContainer extends React.Component {
     this.setState({ [name]: value })
   }
 
-  //Store local state in constant. In 10 seconds, pass local state into checking 
-  //for autosaving or not.
-
-  startCycle = () => {
-    const stateA = this.state
-    setTimeout(()=> this.checkCycle(stateA), 10000)
-  }
-
-  //Compare passed in state from 10 seconds ago to current state set as variable.
-  //If same, start new cycle. If note has been changed, trigger autosave.
-
-  checkCycle(stateA) {
-  const stateB= this.state 
-  stateA === stateB ? this.startCycle() : this.autoSave()
-  }
-
-  //Mirrors submission of form, except call to save action for re-render on other
-  //component on page of new note through .save and starting a new checking cycle 
-  //for seeing if note should autosave.
-  
-  autoSave = (e) => {
-  const values = this.state;
-  const id = this.props.id
-  this.props.actions.save(values, id)
-  this.startCycle()
-}
-
-  
   //Passes in history through withRouter, the state, and the id of the note, 
   //instead of submitting form regularly, calls action to update note with that data.
 
@@ -100,10 +70,8 @@ class EditNoteContainer extends React.Component {
             value={this.state.body}
             onChange = {(e) => {this.handleChange(e)}}
             required />
-          <button>Submit</button>
+          <Button>Submit</Button>
         </form>
-        Protip: This page will autosave your note if it is edited every 10 seconds. 
-        Just watch!
       </div>
     )}
   }
