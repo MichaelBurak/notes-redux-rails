46bcd3479db65ac38864993075a33a638cae3046
Michael Burak <michaelrburak@gmail.com>
2018-02-12 17:23:00 -0500

    updatednote conditionally renders loading

--- client/src/components/UpdatedNote.js
+++ client/src/components/UpdatedNote.js
@@ -4,20 +4,31 @@
 import React from 'react'
 import {connect} from 'react-redux';
 
-const UpdatedNote = ({note}) => <div>
+const UpdatedNote = ({note, loading}) => {
+
+return(
+<div>
+  {loading? 
+  <h1>Loading page...</h1>:
+  <div>
   <h1>
     You have updated your note! Here is the updated note -
   </h1>
   <h3>Title: {note.title}</h3>
   <p>{note.body}</p>
   This page will re-render in 10 seconds.
+  </div>
+  }
 </div>
+)
+}
 
 const mapStateToProps = (state, ownProps) => {
   const note = state.notePad.notes.find(note => note.id == ownProps.match.params.id)
+  const loading = state.notePad.loading 
 
   if (note) {
-    return {note}
+    return {note, loading}
   } else {
     return {note: {}}
   }
