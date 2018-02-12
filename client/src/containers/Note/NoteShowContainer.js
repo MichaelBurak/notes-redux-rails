4124ac2d8068b7592ed6ff05c42355352371fd7e
Michael Burak <michaelrburak@gmail.com>
2018-02-12 17:24:33 -0500

    noteshowcontainer now more comprehensively passes to children presentationals on note show page, loading conditional

--- client/src/containers/Note/NoteShowContainer.js
+++ client/src/containers/Note/NoteShowContainer.js
@@ -1,24 +1,53 @@
 import React from 'react';
 import Note from '../../components/Note';
+import DeleteNote from '../../components/DeleteNote';
+import EditNoteContainer from './EditNoteContainer';
 import {connect} from 'react-redux';
+import * as actions from '../../store/actions/noteActions.js'
+import {bindActionCreators} from 'redux'
 
-// Introspects through router for corresponding note to URL path, passes down
-// appropriate data as note constant to Note presentational.
+class NoteShowContainer extends React.Component {
 
-const NoteShowContainer = (props) => < div > {
-  props.loading
+  render() {
+    return (
+      < div > 
+      {this.props.loading
     ? <h1>Loading page...</h1>
-    : <Note note={props.note}/>
-} </div>
+    : 
+      <div>
+      <Note 
+        note={this.props.note}/>
+      <DeleteNote 
+        note={this.props.note}
+        actions={this.props.actions}/>
+      <EditNoteContainer
+          id={this.props.note.id}
+          title={this.props.note.title}
+          body={this.props.note.body}/>
+        </div>
+      }
+      </div>
+    )
+  }
+
+}
+
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   const note = state.notePad.notes.find(note => note.id == ownProps.match.params.id)
+  const loading = state.notePad.loading
 
   if (note) {
-    return { note }
+    return { note, loading }
   } else {
     return { note: {} }
   }
 }
 
-export default connect(mapStateToProps)(NoteShowContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(NoteShowContainer);
