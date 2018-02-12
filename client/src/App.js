55050ca81d338f4ba2944622fdf7d8306a77ca4b
Michael Burak <michaelrburak@gmail.com>
2018-02-12 17:19:54 -0500

    App.js is now const and has path for errors.

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,29 @@
-//Main App component. Displays navigation bar on all displayed components and handles 
-//routing.
-
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from 'react'
+import {Router, Route, Switch} from 'react-router-dom'
 import NotesIndexContainer from './containers/Note/NotesIndexContainer';
 import NoteShowContainer from './containers/Note/NoteShowContainer';
 import CreateNoteContainer from './containers/Note/CreateNoteContainer';
 import UpdatedNote from './components/UpdatedNote'
 import DeletedNote from './components/DeletedNote'
 import NavigationBar from './components/NavigationBar'
+import ErrorAlert from './components/ErrorAlert'
+import history from './history'
 
-class App extends Component {
-
-  render() {
-    return (
-    <Router> 
-    <div>
-    <NavigationBar />
-    <Switch>
-    <Route path="/" exact component={NotesIndexContainer} />
-    <Route exact path={`/notes/new`} component={CreateNoteContainer} />
-    <Route exact path={`/notes/:id/edited`} exact component= {UpdatedNote} />
-    <Route exact path={`/notes/:id/deleted`} exact component= {DeletedNote} />
-    <Route path={`/notes/:id`} component={NoteShowContainer} />
-    </Switch>
-    </div>    
+const App = ({store}) => (
+    <Router history={history}>
+      <div>
+        < NavigationBar/>
+        <Switch>
+          <Route path="/" exact component={NotesIndexContainer}/>
+          <Route exact path={`/notes/new`} component={CreateNoteContainer}/>
+          <Route exact path={`/notes/:id/edited`} exact component={UpdatedNote}/>
+          <Route exact path={`/notes/:id/deleted`} exact component={DeletedNote}/>
+          <Route path={`/notes/:id`} component={NoteShowContainer}/>
+          <Route path={`/error/`} component={ErrorAlert}/>
+        </Switch>
+      </div>
     </Router>
-    );
-  }
-}
+)
 
-export default App 
+
+export default App
