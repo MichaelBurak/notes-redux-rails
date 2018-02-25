0df74655e01f104b469da2facb1b7ab697d624aa
Michael Burak <michaelrburak@gmail.com>
2018-02-25 15:18:58 -0500

    I forgot how git works

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110122703) do
+ActiveRecord::Schema.define(version: 20180225190412) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "category"
   end
 
 end
