2e343c1d7b146855dde796d7e507597d07fc2705
Michael Burak <michaelrburak@gmail.com>
2018-04-07 13:24:33 -0400

    a bunch more styled components

--- client/src/containers/Note/NotesIndexContainer.js
+++ client/src/containers/Note/NotesIndexContainer.js
@@ -1,44 +1,41 @@
 //Main index page displayed on root of all notes.
 
 import React from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import Notes from '../../components/Notes'
-import * as actions from '../../store/actions/noteActions'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Notes from '../../components/Notes';
+import * as actions from '../../store/actions/noteActions';
+import { List, Grid } from 'semantic-ui-react';
 
 class NotesIndexContainer extends React.Component {
+	// Renders Notes presentational with data passed down through store to props
+	// below.
 
-  // Renders Notes presentational with data passed down through store to props
-  // below.
-
-  render() {
-    return (
-      <div>
-        {this.props.loading
-          ? <h1>Loading page...</h1>
-          : <div>
-          <Notes notes={this.props.notes}/>
-          </div>
-}
-      </div>
-    )
-  }
+	render() {
+		return (
+			<Grid>
+				<List divided verticalAlign="middle">
+					<Notes notes={this.props.notes} />
+				</List>
+			</Grid>
+		);
+	}
 }
 
 //Gives access to notes in store and loading from store for conditional rendering.
 
 function mapStateToProps(state) {
-  return {notes: state.notePad.notes, loading: state.notePad.loading}
+	return { notes: state.notePad.notes, loading: state.notePad.loading };
 }
 
 //Gives access to actions, specifically fetchNotes() in this case.
 
 function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(actions, dispatch)
-  }
+	return {
+		actions: bindActionCreators(actions, dispatch),
+	};
 }
 
 //Connects to store.
 
-export default connect(mapStateToProps, mapDispatchToProps)(NotesIndexContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(NotesIndexContainer);
